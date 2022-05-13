a0ee60678d30de909a933aac02e67b0eb10a63e3
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-05-13 11:27:52 +0900

    모든 번수나 함수는 CamelCase

---
+++ src/com/company/example1/NamingConvention/UserService.java
@@ -0,0 +1,14 @@
+package com.company.example1.NamingConvention;
+
+
+/**
+ * 모든 변수나 함수, 클래스들은 CamelCase 스타일로 한다. (클래스는 첫자가 대문자)
+ */
+public class UserService {
+	private String firstName;
+
+	public String getFirstName(){
+		return firstName;
+	}
+}
+
