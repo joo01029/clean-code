c3dbfb45fafeb3c9f4fb45f212fb3e4e79d67eca
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-05-13 14:53:16 +0900

    너무 긴 글자를 함수명이나 변수명으로 사용 지양

---
+++ src/com/company/example1/NamingConvention/UserRepository.java
@@ -0,0 +1,16 @@
+package com.company.example1.NamingConvention;
+
+public class UserRepository {
+
+	// 너무 긴 글자를 변수명이나/ 메소드명으로 사용하지 말아라
+	public void saveUserIntoMongoDB(User user) {
+
+	}
+	public void saveUser(User user){
+
+	}
+
+	private class User{
+
+	}
+}
